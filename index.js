74d1f537450be27f2b6b414677527adc7a1319db
Katie Belanger <belangerkat@gmail.com>
2019-12-11 21:47:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(STRING) {
   return STRING.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string)
+  return string.toUpperCase
 }
